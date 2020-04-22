7ad23bf19ecf89ebacd86387cd20d7968a24eb67
Freddy-kim <dyddnssla@naver.com>
2020-04-22 23:29:25 +0900

    First Git Test

---
+++ CalssHistory/src/class0/Class1.java
@@ -0,0 +1,20 @@
+package class0;
+
+public class Class1 {
+	
+	public static void main(String[] args) {
+		
+		String str = "Hello Git";
+		System.out.println(str);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}//main
+
+}
